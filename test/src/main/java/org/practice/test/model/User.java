3bf462b316e1ffc41c1088aadf8f65f8cae64abc
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-17 20:07:02 +0500

    user model

---
+++ test/src/main/java/org/practice/test/model/User.java
@@ -0,0 +1,36 @@
+package org.practice.test.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public User() {
+    }
+    
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+    
+}
