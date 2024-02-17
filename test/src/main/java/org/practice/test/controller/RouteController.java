5bcd938405a50abb6f2ef49dfe2eb593d8490cbe
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-17 19:51:29 +0500

    Update

---
+++ test/src/main/java/org/practice/test/controller/RouteController.java
@@ -0,0 +1,12 @@
+package org.practice.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouteController {
+    @GetMapping("/")
+    public String responseData() {
+        return "Success";
+    }
+}
