3debe667082705d8e5d9553920e9fc323fc241a5
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-19 00:35:28 +0500

    token generator

--- test/src/main/java/org/practice/test/config/JwtAuthenticationFilter.java
+++ test/src/main/java/org/practice/test/config/JwtAuthenticationFilter.java
@@ -1,31 +1,57 @@
 package org.practice.test.config;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        var jwtToken = getTokenFromRequest(request);
+        var jwtTokenOptional = getTokenFromRequest(request);
+
+        jwtTokenOptional.ifPresent(jwtToken -> {
+            if (JwtUtils.validateToken(jwtToken)) {
+                var username = JwtUtils.getUsernameFromToken(jwtToken);
+
+                var userDetails = userDetailsService.loadUserByUsername((String) username);
+              
+                var authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+                        userDetails.getAuthorities());
+                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            }
+        });
+
+        filterChain.doFilter(request, response);
     }
 
-        private String getTokenFromRequest(HttpServletRequest request){
+        private Optional<String> getTokenFromRequest(HttpServletRequest request){
             var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
           if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
-                return authHeader.substring(7);
+                return Optional.of(authHeader.substring(7));
             }
 
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+            return Optional.ofNullable("Unimplemented method 'doFilterInternal'");
     }
  
 }
