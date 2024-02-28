c5225ee2e6f3e57222cec04eccf07a2e137d2e95
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-28 22:36:24 +0500

    new info

--- Authorization/src/main/java/com/auth/Authorization/Config/SecurityConfig.java
+++ Authorization/src/main/java/com/auth/Authorization/Config/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.auth.Authorization.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -29,29 +28,9 @@ public class SecurityConfig {
     public SecurityFilterChain webSecurity(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.securityMatcher(new AntPathRequestMatcher("/api/**")).csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).userDetailsService(userInfoService)
-        .httpBasic(withDefaults())
+        
                 .build();
     }
 
-
-
-    private Customizer<HttpBasicConfigurer<HttpSecurity>> withDefaults() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'withDefaults'");
-    }
-
-
-
-    @Order(2)
-    @Bean
-    public SecurityFilterChain h2ConsoleSecurityFilterChainConfig(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-                .securityMatcher(new AntPathRequestMatcher(("/h2-console/**")))
-                .authorizeHttpRequests(auth->auth.anyRequest().permitAll())
-                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
-                
-                .build();
-    }
-   
 
 }
